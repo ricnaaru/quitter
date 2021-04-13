7e465d18d03330789d7f972605f6390b6e806521
ricnaaru <richardothayeb@gmail.com>
2021-04-13 16:32:25 +0700

    migrate to flutter 2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 2.0.0
+
+* Migrate to Flutter 2
+
 ## 1.0.0
 
 * Initial release.
--- android/src/main/java/com/ric/quitter/QuitterPlugin.java
+++ android/src/main/java/com/ric/quitter/QuitterPlugin.java
@@ -3,36 +3,61 @@ package com.ric.quitter;
 import android.app.Activity;
 import android.os.Build;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** QuitterPlugin */
-public class QuitterPlugin implements MethodCallHandler {
-  private Activity activity;
+public class QuitterPlugin implements FlutterPlugin, ActivityAware, MethodChannel.MethodCallHandler {
+    private MethodChannel channel;
+    @Nullable
+    private Activity activity;
 
-  public QuitterPlugin(Activity activity) {
-    this.activity = activity;
-  }
-
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "quitter");
-    channel.setMethodCallHandler(new QuitterPlugin(registrar.activity()));
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("quitApplication")) {
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        activity.finishAndRemoveTask();
-      } else {
-        activity.finishAffinity();
-      }
-    } else {
-      result.notImplemented();
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        channel = new MethodChannel(binding.getBinaryMessenger(), "quitter");
+        channel.setMethodCallHandler(this);
     }
-  }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        activity = null;
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        activity = null;
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
+        if (call.method.equals("quitApplication")) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                activity.finishAndRemoveTask();
+            } else {
+                activity.finishAffinity();
+            }
+        } else {
+            result.notImplemented();
+        }
+    }
 }
